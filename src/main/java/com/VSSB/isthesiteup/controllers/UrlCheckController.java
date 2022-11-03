8621689a393d7becd050083705f2129ff689094b
suliaax <arthur.sulian@abbott.com>
2022-11-03 16:49:31 -0700

    added comments
    localhost:8080/check?url=https://google.com

--- src/main/java/com/VSSB/isthesiteup/controllers/UrlCheckController.java
+++ src/main/java/com/VSSB/isthesiteup/controllers/UrlCheckController.java
@@ -15,25 +15,27 @@ public class UrlCheckController {
     private final String SITE_IS_DOWN = "Site is down!";
     private final String INCORRECT_URL = "URL is incorrect!";
 
+    //usage:
+    //localhost:8080/check?url=https://google.com
     
     @GetMapping("/check")
     public String getUrlStatusMessage(@RequestParam String url){
         String returnMessage = "";
         try {
-            URL urlObj = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            int responseCodeValue = conn.getResponseCode() / 100;
+            URL urlObj = new URL(url); //gets url from omnibox
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection(); //opens connection to site
+            conn.setRequestMethod("GET"); //sets method of url request to get data
+            conn.connect(); //attempts to connect
+            int responseCodeValue = conn.getResponseCode() / 100;  // if response code is in 200 - 300 range, site is up
             if (responseCodeValue == 2 || responseCodeValue == 3){
                 returnMessage = SITE_IS_UP;
             }
             else{
                 returnMessage = SITE_IS_DOWN;
             }
-        } catch (MalformedURLException e) {
+        } catch (MalformedURLException e) { // bad url
             returnMessage = INCORRECT_URL;
-        } catch (IOException e) {
+        } catch (IOException e) { //IO issue means site is down
             returnMessage = SITE_IS_DOWN;
         }
 
